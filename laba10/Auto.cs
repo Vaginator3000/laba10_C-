ee368d104b041472c66d7be0f1b886de587efb29
Vladimir <Shifman.1704@mail.ru>
2020-12-24 17:16:20 +0700

    Добавьте файлы проекта.

---
+++ laba10/Auto.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba10
+{
+    class Auto
+    {
+        String brand; //марка
+        int power; //цвет
+        int cost;     //стоимость
+        public Auto()
+        {
+            brand = "";
+            power = 0;
+            cost = 0;
+        }
+
+        public Auto(String brand)
+        {
+            this.brand = brand;
+            power = 0;
+            cost = 0;
+        }
+
+        public Auto(String brand, int power, int cost)
+        {
+            this.brand = brand;
+            this.cost = cost;
+            this.power = power;
+        }
+
+        public void display()
+        {
+            try
+            {
+                if (brand.Equals("")) throw new Exception("Ошибка! Не указана марка!");
+                Console.WriteLine($"{brand}, {power} - {cost}p");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void read()
+        {
+            Console.WriteLine("Марка: ");
+            brand = Console.ReadLine();
+            Console.WriteLine("Мощность: ");
+            power = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Стоимость: ");
+            cost = Convert.ToInt32(Console.ReadLine());
+        }
+
+    }
+}
---
+++ laba10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto car1 = new Auto("Ford", 400, 300000);
+            Auto car2 = new Auto("Mazda");
+            Auto car3 = new Auto();
+            car1.display();
+            car2.display();
+            car3.display();
+        }
+    }
+}
